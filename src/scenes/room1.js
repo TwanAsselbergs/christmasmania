717bd32c656d6e2ecb8b54c7db4a45a6802ba4af
TwanAsselbergs <twanasselbergs@outlook.com>
2025-01-08 12:35:00 +0100

    added imports

--- src/scenes/room1.js
+++ src/scenes/room1.js
@@ -1,5 +1,14 @@
 import { makePlayer } from "../entities/player.js";
-import { setBackgroundColor, setMapColliders } from "./roomUtils.js";
+import { makeDrone } from "../entities/enemyDrone.js";
+import {
+  setBackgroundColor,
+  setCameraZones,
+  setMapColliders,
+  setCameraControls,
+} from "./roomUtils.js";
+import { state } from "../state/globalStateManager.js";
+import { makeBoss } from "../entities/enemyBoss.js";
+import { makeCartridge } from "../entities/healthCartridge.js";
 
 export function room1(k, roomData) {
   setBackgroundColor(k, "#a2aed5");
@@ -14,7 +23,13 @@ export function room1(k, roomData) {
 
   const colliders = [];
   const positions = [];
+  const cameras = [];
+
   for (const layer of roomLayers) {
+    if (layer.name === "cameras") {
+      cameras.push(...layer.objects);
+    }
+
     if (layer.name === "positions") {
       positions.push(...layer.objects);
       continue;
@@ -26,13 +41,36 @@ export function room1(k, roomData) {
   }
 
   setMapColliders(k, map, colliders);
+  setCameraZones(k, map, cameras);
 
   const player = k.add(makePlayer(k));
+  setCameraControls(k, player, map, roomData);
 
   for (const position of positions) {
     if (position.name === "player") {
       player.setPosition(position.x, position.y);
       player.setControls();
+      player.setEvents();
+      player.enablePassthrough();
+      continue;
+    }
+
+    if (position.type === "drone") {
+      const drone = map.add(makeDrone(k, k.vec2(position.x, position.y)));
+      drone.setBehavior();
+      drone.setEvents();
+      continue;
+    }
+
+    if (position.name === "boss" && !state.current().isBossDefeated) {
+      const boss = map.add(makeBoss(k, k.vec2(position.x, position.y)));
+      boss.setBehavior();
+      boss.setEvents();
+      continue;
+    }
+
+    if (position.type === "cartridge") {
+      map.add(makeCartridge(k, k.vec2(position.x, position.y)));
     }
   }
 }
